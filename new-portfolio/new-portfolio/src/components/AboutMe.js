819c144de725faebaebff477c325c212bef1629c
wjimh314 <wjimh314@gmail.com>
2022-09-20 16:31:07 -0400

    edits to about me

--- new-portfolio/new-portfolio/src/components/AboutMe.js
+++ new-portfolio/new-portfolio/src/components/AboutMe.js
@@ -6,8 +6,9 @@ const AboutMe = () => {
 		<div className="container py-5">
 			<div className="row">
 				<div className="col-lg-6 col-xm-12">
-					<div className="photo-wrap mb-5">
-						<img src={Me} alt="me" />;
+                <div className="photo-wrap">
+				<img  className="profile-img"src={Me} alt="me" />
+                       
 					</div>
 				</div>
 				<div className="col-lg-6 col-xm-12">
