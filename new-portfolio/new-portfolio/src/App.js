8eef286fd35e76454b2cb74c3ff61c0017865302
wjimh314 <wjimh314@gmail.com>
2022-09-20 10:08:38 -0400

    added navbar

--- new-portfolio/new-portfolio/src/App.js
+++ new-portfolio/new-portfolio/src/App.js
@@ -1,9 +1,10 @@
 
 import './App.css';
 import "bootstrap/dist/css/bootstrap.min.css";
+import Navigation from "./components/Naviagation"
 function App() {
   return (
-    <h2>Portfolio Project</h2>
+    <Navigation/>
 
   );
 }
