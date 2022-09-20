7ad50689edf6ba0ab89ea693000b2f1e35f6e798
wjimh314 <wjimh314@gmail.com>
2022-09-20 17:16:03 -0400

    started working on project portfolio

---
+++ new-portfolio/new-portfolio/src/components/portfolio.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import foodFREQs from '../images/foodFREQS.jpg';
+import gitthatcurrency from '../images/gitthatcurrency.png';
+export const portfolio = () => {
+  return (
+    <div className="portfolio-wrapper">
+    <div className= "container">
+        <h1 className="text-uppercase text-center py-5">portfolio</h1>
+        <div className='image-box-wrapper row justify-content-center'>
+            <img className='="portfolio-image'
+        </div>
+
+    </div>
+
+    </div>
+  )
+}
